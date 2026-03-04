e11f2a1a466c19c23434b187f6527e6853123d5a
pedrobidel157 <pedro.nascimento3@estudante.ifms.edu.br>
2026-03-04 12:20:52 -0400

    Aula3

---
+++ backend/src/todos/dto/create-todo.dto.ts
@@ -0,0 +1,18 @@
+//import {TodoPriority} from 'generated/prisma/enums';
+
+export class CreateTodoDto {
+    title: string;
+    description: string;
+    completed: boolean;
+    priority: TodoPriotity;
+    dueAt: Date;
+    completedAt: Date
+
+    userID: string;
+    createdAT: Date;
+    updatedAT: Date;}
+
+enum TodoPriotity {LOW= 'LOM',
+      MEDIUM= 'MEDIUM',
+      HIGH='HING',}
+
